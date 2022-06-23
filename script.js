c21939795a2c0abfab736c61eb807684bffefe12
ukakit <106052696+ukakit@users.noreply.github.com>
2022-06-23 14:33:28 -0700

    initial set up of html

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+
+hamburgerMenu.addEventListener('click', ()=> console.log('test'))
+
+
+const landingName = document.querySelector("#landing-name")
+
+landingName.addEventListener('click', ()=> landingName.classList.toggle('hidden'))
+
