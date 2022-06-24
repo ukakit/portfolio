f16b2aaaf35f021b4973ede45b0acd7e425be332
ukakit <106052696+ukakit@users.noreply.github.com>
2022-06-24 16:48:27 -0700

    added modals

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const aboutMePage = document.querySelector(".about-me-container");
 const projectPage = document.querySelector(".projects-container");
 const contactMePage = document.querySelector(".contact-me-container")
 const sidenav = document.querySelector(".sidenav");
+// modals
+const projectOneModal = document.querySelector('#modalOne')
+const openProjectOneModal = document.querySelector('#openModalOne');
+const closeProjectOneModal = document.querySelector('#close')
+
+const projectTwoModal = document.querySelector('#modalTwo')
+const openProjectTwoModal = document.querySelector('#openModalTwo');
+const closeProjectTwoModal = document.querySelector('#close')
+
+const projectThreeModal = document.querySelector('#modalThree')
+const openProjectThreeModal = document.querySelector('#openModalThree');
+const closeProjectThreeModal = document.querySelector('#close')
 
 const allPage = [landingPage,aboutMePage,projectPage,contactMePage]
 // const contactMeNav = document.querySelector("#landing-name")
@@ -13,6 +25,7 @@ const allPage = [landingPage,aboutMePage,projectPage,contactMePage]
 // const resumeNav = document.querySelector("#landing-name")
 
 // resumeNav.addEventListener('click', ()=> resumeNav.classList.toggle('hidden'))
+
 
 
 const openNav = function() {
@@ -33,6 +46,28 @@ const backToLanding = function() {
     closeNav()
 }
 
+const openModalOne = () => {
+    modalOne.style.display = 'block';
+  }
+
+const closeModalOne = () => {
+    modalOne.style.display = 'none'
+}
+
+const openModalTwo = () => {
+    modalTwo.style.display = 'block';
+  }
+
+const closeModalTwo = () => {
+    modalTwo.style.display = 'none'
+}
+const openModalThree = () => {
+    modalThree.style.display = 'block';
+  }
+
+const closeModalThree = () => {
+    modalThree.style.display = 'none'
+}
 const openAboutMe = function() {
     for (page of allPage) {
         if (page != aboutMePage) {
